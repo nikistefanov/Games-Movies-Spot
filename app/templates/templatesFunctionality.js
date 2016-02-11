a65af64b82e9406893813b9d50bb208326c008c6
nikistefanov <nikistefanovbg89@gmail.com>
2016-02-11 19:38:29 +0200

    Fixed the validation when adding product.

--- app/templates/templatesFunctionality.js
+++ app/templates/templatesFunctionality.js
@@ -4,7 +4,7 @@ var templatesFunctionality = function() {
     var filename = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
     $(".navbar-nav a[href*=" + filename + "]").attr("id", "active");
 
-    $('#site-slogan-title').text('Your place for buying, selling or giving away games, movies and books!');
+    $('#site-slogan-title').text('Your place for buying, selling or giving away games and movies!');
     $('#site-slogan-text').text('Are you bored with your current games, sell them and buy new ones.');
     $('#site-slogan-btn').css("display", "inline-block");
 
